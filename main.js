815fd8d153264ffd1d350e4d2818da056a04e2f0
marcosferr <ferreiramarcosadrian@gmail.com>
2023-12-07 16:02:27 -0300

    [Tarea] sensei

--- main.js
+++ main.js
@@ -24,3 +24,19 @@ class Ninja {
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+
+class Sensei extends Ninja {
+  constructor(nombre) {
+    super(nombre);
+    this.salud = 200;
+    this.velocidad = 10;
+    this.fuerza = 10;
+    this.sabiduria = 10;
+  }
+  speakWisdom() {
+    super.drinkSake();
+    console.log(
+      "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+    );
+  }
+}
